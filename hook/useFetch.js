1b9f6e295e4a03ec2b830761830b466f04e829ab
Tanish2002Basu <>
2023-03-31 12:11:03 +0530

    added some api connections

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-
+import {Rapid_API_Key} from "@env";
+const rapidApikey = Rapid_API_Key;
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
@@ -10,7 +11,7 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": '',
+      "X-RapidAPI-Key": rapidApikey,
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
